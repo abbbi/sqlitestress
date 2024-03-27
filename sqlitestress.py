d0019cb9fd1305df18a223c223d571e8d0224dcd
ma <ma@sep.de>
2024-03-27 08:38:35 +0100

    add wal mode option

--- sqlitestress.py
+++ sqlitestress.py
@@ -26,10 +26,22 @@ parser.add_argument(
     "--every",
     default=5,
     required=False,
-    help="every X process inserts instead of selects",
+    help="every X process inserts instead of selects, default: %(default)s)",
 )
 parser.add_argument(
-    "-c", "--cycles", default=500, required=False, help="amount of iterations"
+    "-W",
+    "--wal-mode",
+    default="wal",
+    choices=["wal", "wal2"],
+    required=False,
+    help="Wal mode to use, default:  %(default)s)",
+)
+parser.add_argument(
+    "-c",
+    "--cycles",
+    default=500,
+    required=False,
+    help="amount of iterations, default: %(default)s)",
 )
 parser.add_argument(
     "-n",
@@ -43,23 +55,23 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def sqlite_doit(cnt, dbfile, inserts, every, onlyinsert):
+def sqlite_doit(cnt, args):
     """write"""
     local_conn = sqlite3.connect(
-        dbfile,
+        args.dbfile,
         # these two values have great impact on
         # database-locked situations
         isolation_level=None,
         timeout=10,
     )
     cursor = local_conn.cursor()
-    cursor.execute("PRAGMA journal_mode=wal")
+    cursor.execute(f"PRAGMA journal_mode={args.wal_mode}")
 
-    if cnt % int(every):
-        for _ in range(int(inserts)):
+    if cnt % int(args.every):
+        for _ in range(int(args.inserts)):
             cursor.execute("insert into test values('1')")
     else:
-        if not onlyinsert:
+        if not args.onlyinsert:
             cursor.execute("select * from test")
 
     ret = cursor.fetchall()
@@ -77,7 +89,13 @@ def main():
         # setup db
         conn = sqlite3.connect(args.dbfile, isolation_level=None)
         dbcon = conn.cursor()
-        dbcon.execute("PRAGMA journal_mode=wal")
+        dbcon.execute(f"PRAGMA journal_mode={args.wal_mode}")
+        walmode = dbcon.fetchone()[0]
+        print(f"WAL mode as returned by connection: {walmode}")
+        if walmode != args.wal_mode:
+            print(
+                f"Warning: Wal mode {args.wal_mode} requested but Database defaults to: {walmode}"
+            )
         try:
             if args.nodelete is False:
                 dbcon.execute("drop table test")
@@ -92,7 +110,9 @@ def main():
         print(f"Every {args.every}th process inserts: {args.inserts} values")
         fut = {
             executor.submit(
-                sqlite_doit, cnt, args.dbfile, args.inserts, args.every, args.onlyinsert
+                sqlite_doit,
+                cnt,
+                args,
             ): cnt
             for cnt in range(int(args.cycles))
         }
@@ -107,6 +127,7 @@ def main():
                 if cnt % 1000:
                     if (len(data)) > 0:
                         print(f"rows: {len(data)}")
+
 
 if __name__ == "__main__":
     main()
